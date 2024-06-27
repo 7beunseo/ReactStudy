bab8be8c50ea5131bf0f2d2be98c94c758f6d924
7beunseo <kimes0403@gmail.com>
2024-06-27 19:49:34 +0900

    분기와 반복

--- han-ieum/kdt-react/kdt-app/src/App.js
+++ han-ieum/kdt-react/kdt-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Logo from './components/Logo';
 import Paragraph from './components/Paragraph';
+import Board from './components/Board'
+
+import {useState} from "react";
 
 function App() {
   const name = "리엑트";
@@ -8,8 +11,42 @@ function App() {
   const showLogo = "show";
   const names = ["React", "Vue", "Angular"]
 
+  const articles = [{
+      id: 1,
+      title: "안녕하세요1",
+      author: "김은서"
+    },{
+      id: 2,
+      title: "안녕하세요2",
+      author: "김은서"
+    },{
+      id: 3,
+      title: "안녕하세요3",
+      author: "김은서"
+    },{
+      id: 4,
+      title: "안녕하세요4",
+      author: "김은서"
+    },{
+      id: 5,
+      title: "안녕하세요5",
+      author: "김은서"
+    },
+  ]
+
+  // 분기
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
+      <button onClick={() => setVisible(!visible)}>Toggle</button>
+      {visible ? 
+        (<h1>논리곱 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있습니다.</h1>
+
+        ) : null}
+
+        {visible ? <Board articles={articles}/> : <p>게시판을 보려면 Toggle 버튼 클릭</p>}
+
       <div className="App">
         <header className="App-header">
           {/* 3항연산자 */}
