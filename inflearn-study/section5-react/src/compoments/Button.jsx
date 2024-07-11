6b93adaa8dbdd36e056f3d5cb8b4acea594cf395
7beunseo <kimes0403@gmail.com>
2024-07-11 15:14:58 +0900

    이벤트 처리

--- inflearn-study/section5-react/src/compoments/Button.jsx
+++ inflearn-study/section5-react/src/compoments/Button.jsx
@@ -1,9 +1,22 @@
 const Button = ({ text, color, children }) => {
     // console.log(props);
-    return <button style={{color: color}}>
-            {text} - {color.toUpperCase()}
-            {children}
-        </button>;
+
+    // 이벤트 핸들러를 함수로 만듦
+    // 이벤트 객체를 전달함 
+    const onClickButton = (e) => {
+        console.log(e)
+        console.log(text);
+    }
+    return (
+        <button 
+            // 이벤트 핸들러 
+            onClick={onClickButton}
+            style={{color: color}}
+        >
+                {text} - {color.toUpperCase()}
+                {children}
+        </button>
+    );
 };
 
 // props가 없을 경우 처리
