81fb5796a28d0f5986965bf7afb6f09122e950ed
7beunseo <kimes0403@gmail.com>
2024-07-11 15:39:16 +0900

    State로 상태 관리

--- inflearn-study/section5-react/src/App.jsx
+++ inflearn-study/section5-react/src/App.jsx
@@ -1,28 +1,15 @@
 import './App.css'
-import Header from './compoments/Header.jsx';
-import Main from './compoments/Main.jsx';
-import Footer from './compoments/Footer.jsx';
-import Button from "./compoments/Button.jsx"
+import { useState } from "react";
+import Bulb from './compoments/Bulb';
+import Counter from './compoments/Counter';
 
 function App() {
-  // ... 연산자와 함께 사용하여 개별로 컴포넌트의 props에 전달해주게 됨 
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2, 
-    c: 3
-  };
 
   return (
     <>
-      <Button {...buttonProps}/>   {/* 객체 형태로 전달 */}
-      <Button text={"카페"}/>
-
-      {/* 컴포넌트를 props로 전달할 수도 있음 - children이라는 props로 전달됨 */}
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <Bulb />
+      <Counter />
+      
     </>
   )
 }
