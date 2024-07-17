dfd8c82e1738b02da5159fe5329a4721566a2094
7beunseo <kimes0403@gmail.com>
2024-07-17 15:41:17 +0900

    useReducer를 사용하여 투두리스트 업그레이드

--- inflearn-study/section9-react/src/App.jsx
+++ inflearn-study/section9-react/src/App.jsx
@@ -2,48 +2,34 @@ import './App.css'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
-import Exam from './components/Exam'
-import { useState, useRef } from 'react'
+import { useState, useRef, useReducer } from 'react'
 
 function App() {
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current ++, // 하나 만들때마다 1 증가
-      isDone: false,
-      content: content,
-      date: new Date().getTime()
-    }
-
-    // todos.push(newTodo); -> 상태 변화 함수를 호출해야만 한다. 
-    setTodos([newTodo, ...todos])
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current ++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime()
+      }
+    })
   }
 
   const onUpdate = (targetId) => {
-    // todos State 값들 중에
-    // targetID와 일치하는 ID를 갖는 투두 아이템의 isDone 변경
-    
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 바꾼 새로운 배열 
-    setTodos(todos.map((todo) => {
-      /*
-      if(todo.id === targetId) {
-        return {
-          ...todo,
-          isDone: !todo.isDone
-        }
-      }
-      return todo;
-    })) */
-
-      return todo.id === targetId 
-      ? { ...todo, isDone: !todo.isDone }
-      : todo
-      
-    }))}
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId
+    })  
+  }
 
   const onDelete = (targetId) => {
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열 
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId
+    })
   }
 
   const mockData = [
@@ -67,17 +53,30 @@ function App() {
     },
   ]
 
-  const [todos, setTodos] = useState(mockData);
+  function reducer(state, action) {
+    switch(action.type) {
+      case 'CREATE': return [action.data, ...state];
+      case "UPDATE": return state.map((item) => 
+        item.id === action.targetId
+        ? {...item, isDone: !item.isDone }
+        : item
+      )
+      case "DELETE": return state.filter((item) => item.id !== action.targetId);
+      default: return state;
+    }
+  }
+
+  // const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3); // id 값 저장 
 
   return (
     <div className='App'>
-      <Exam />
-      {/*
+      
       <Header />
       <Editor onCreate={onCreate}/>
       <List onUpdate={onUpdate} todos={todos} onDelete={onDelete}/>
-      */}
+      
     </div>
   )
 }
