1c3456187e0133e5c858d74fa86abbb53e9cd4af
7beunseo <kimes0403@gmail.com>
2024-07-17 17:25:40 +0900

    useMemo를 사용하여 연산 최적화

--- inflearn-study/sectino10-react/src/components/List.jsx
+++ inflearn-study/sectino10-react/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
     const [search, setSearch] = useState("")
@@ -19,6 +19,7 @@ const List = ({ todos, onUpdate, onDelete }) => {
     const filteredTodos = getFillteredData();
 
     // 최적화
+    /*
     const getAnalyzedData = () => {
         console.log("getAnalyzedData 호출!");
         const totalCount = todos.length;
@@ -32,9 +33,29 @@ const List = ({ todos, onUpdate, onDelete }) => {
             doneCount,
             notDoneCount
         }
-    }
+    } */
 
-    const {totalCount, doneCount, notDoneCount} = getAnalyzedData(); // 리스트 컴포넌트가 리렌더링될때마다 계속 호출됨 (search)
+    // useMemo 사용
+    const {totalCount, doneCount, notDoneCount} = useMemo(() => {
+        console.log("getAnalyzedData 호출!");
+        const totalCount = todos.length;
+        // todo들 중에 된 것들만 저장 
+        const doneCount = todos.filter((todo) => todo.isDone).length // todo의 length가 길어질 수록 시간이 오래 걸림 
+        // 완료되지 않은 todo
+        const notDoneCount = totalCount - doneCount;
+
+        // 그대로 반환 
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        }
+    }, [todos])
+    // 두번째 배열이 의존성 배열, deps임 
+    // deps에 포함된 값이 변경되었을 때만 콜백함수를 다시 실행함 
+    // 해당 콜백이 반환하는 값을 그대로 반환함 
+
+    // const {totalCount, doneCount, notDoneCount} = getAnalyzedData(); // 리스트 컴포넌트가 리렌더링될때마다 계속 호출됨 (search)
 
     return <div className="List">
         <h4>Todo List 🌱</h4>
