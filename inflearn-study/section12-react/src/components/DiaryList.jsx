a69789de0cce182ed72c3517f7f1c0b89dda8b1d
7beunseo <kimes0403@gmail.com>
2024-07-25 14:39:28 +0900

    최신순, 오래된 순 정렬

--- inflearn-study/section12-react/src/components/DiaryList.jsx
+++ inflearn-study/section12-react/src/components/DiaryList.jsx
@@ -2,14 +2,32 @@ import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import './DiaryList.css'
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const DiaryList = ({ data }) => {
     const nav = useNavigate();
+    const [sortType, setSortType] = useState("latest"); // 초기값을 최신순으로 설정
+
+    const onChnageSortType = (e) => {
+        setSortType(e.target.value);
+    }
+
+    const getSortedDate = () => {
+        return data.toSorted((a, b) => {
+            if(sortType === "oldest") 
+                return Number(a.createdDate) - Number(b.createdDate);
+            else 
+                return Number(b.createdDate) - Number(a.createdDate);
+        });
+    }
+
+    const sortedData = getSortedDate();
+
     return (
         <div className="DiaryList">
             {/* 메뉴바 */}
             <div className="menu_bar">
-                <select>
+                <select onChange={onChnageSortType}>
                     <option value={"latest"}>최신순</option>
                     <option value={"oldest"}>오래된 순</option>
                 </select>
@@ -18,7 +36,7 @@ const DiaryList = ({ data }) => {
             </div>
             {/* 메뉴 아이템 */}
             <div className="list_wrapper">
-                {data.map((item) => <DiaryItem key={item.id} {...item} />)}
+                {sortedData.map((item) => <DiaryItem key={item.id} {...item} />)}
             </div>
         </div>
 
