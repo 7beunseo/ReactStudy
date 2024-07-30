c19515e89511f48069a06628cb36b5d54494b0c5
7beunseo <kimes0403@gmail.com>
2024-07-30 14:02:52 +0900

    웹 스토리지 이용하기

--- inflearn-study/section12-react/src/App.jsx
+++ inflearn-study/section12-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useRef, createContext } from 'react'
+import { useReducer, useRef, createContext, useEffect, useState } from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
@@ -43,20 +43,77 @@ export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function reducer(state, action) {
+  let nextStage;
   switch(action.type) {
-    case "CREATE": return [action.data, ...state];
-    case "UPDATE": return state.map((item) => 
-      String(item.id) === String(action.data.id) ? action.data : item
-    );
-    case "DELETE": return state.filter((item) => String(item.id) !== String(action.id));
-    default: return state;
+    case "INIT": {
+      // 로컬 스토리지에 한번 더 보관할 필요가 없음 (어차피 같은 값임)
+      return action.data;
+    }
+    case "CREATE": {
+      nextStage = [action.data, ...state];
+      break;
+    }
+    case "UPDATE": {
+      nextStage = state.map((item) => 
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+      break;
+    } 
+    case "DELETE": {
+      nextStage = state.filter((item) => String(item.id) !== String(action.id));
+      break;
+    }
+    default: nextStage = state;
   }
+  
+  localStorage.setItem("diary", JSON.stringify(nextStage));
+  return nextStage;
 }
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(4);
-   
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태 
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+   useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      if (Array.isArray(parsedData)) {
+        let maxId = 0;
+        parsedData.forEach((item) => {
+          if (Number(item.id) > maxId) {
+            maxId = Number(item.id);
+          }
+        });
+        idRef.current = maxId + 1;
+
+        dispatch({
+          type: "INIT",
+          data: parsedData
+        });
+        setIsLoading(false);
+      }
+      else {
+        setIsLoading(false);
+      }
+    }
+    else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  // 로컬스토리지에 데이터를 저장하는 방법 
+  // localStorage.setItem("test", "hello"); // 키 값과 value 값을 지정함 
+  // 주석처리해도 사라지지 않음  
+  // localStorage.setItem("person", JSON.stringify({name: "김은서"})) // 객체를 그대로 로컬 스토리지에 보관하면 안됨, 객체를 문자열 형태로 바꿔야 함 
+
+  // 로컬스토리지에서 값 불러오기 
+  console.log(JSON.parse(localStorage.getItem("person"))) // 다시 객체 형태로 변환, undeifind이면 안됨 
+
+  // 로컬스토리지에서 값 삭제 
+  localStorage.removeItem("test");
+
   // 새로운 일기 추가
   const onCreate =(createdDate, emotionId, content) => {
     dispatch({
@@ -89,6 +146,10 @@ function App() {
       type: "DELETE",
       id
     })
+  }
+
+  if(isLoading) {
+    return <div>데이터 로딩 중입니다 ...</div>
   }
 
   return (
